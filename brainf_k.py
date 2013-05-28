54aad3fde81e5db538fa4027ba529eb93fb30c73
Sayan Paul <sayan_666@yahoo.com>
2013-05-28 17:42:08 +0530

    The python file added

---
+++ brainf_k.py
@@ -0,0 +1,44 @@
+import sys
+ls=sys.stdin.readlines()
+p=""
+for l in ls:
+    for c in l:
+        if c=='%':
+            break
+        if ord(c)!=10 and ord(c)!=13 :
+            p+=c
+f,ct,pt,i=0,0,0,0
+t,ans,stk=[0]*32768,"",[]
+while(i<len(p)):
+    c=p[i]
+    if c=='+':
+       t[pt]+=1
+       t[pt]%=256
+    if c=='-':
+       t[pt]-=1
+       if t[pt]<0:
+           t[pt]=255
+    if c=='<':
+        pt-=1
+        if pt<0:
+            pt=32767
+    if c=='>':
+        pt+=1
+  	pt%=32768
+    if c=='.':
+        ans+=chr(t[pt])
+    if c=='[':
+        ct+=1
+        stk.append(i)
+    if c==']':
+        ct-=1
+        if ct<0:
+            f=1
+            break
+        if t[pt]:
+            i=stk[-1]
+            continue
+        else:
+            o=stk.pop()
+    i+=1
+print f?"COMPILE ERROR":ans
